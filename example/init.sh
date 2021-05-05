812521fbc5772546d1f3a17c3139230a49c28f81
Maik Laschober <webmaster@omega-center.com>
2021-05-05 10:14:05 +0200

    added more stuff to init file (todo: test)

--- example/init.sh
+++ example/init.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# create target directory if it does not exist
+mkdir -p /appdata/rising-world-java/dedicated-server
+# copy files from data to game directory
 rsync -rltDvzr ./data/* /appdata/rising-world-java/dedicated-server
+# set user:group to copied files
+chown -R 1000:1000 /appdata/rising-world-java/dedicated-server
+
+# start service
+docker compose up -d
